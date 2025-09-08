2d60d486688f6cf83aa84e4ba5d14c3e70ea1cad
PETERS <cp0973791@dt050565.otc.edu>
2025-09-08 09:05:15 -0500

    functions without parameters & w parameters

--- functions.php
+++ functions.php
@@ -1,3 +1,19 @@
 <?php
-echo "PHP Functions Notes"
+    echo "PHP Functions Notes\n";
+
+    //function with no parameters
+    function displayWelcomeMessage(){
+        echo("Welcome to my Demo!\n");
+    }
+
+    displayWelcomeMessage();
+
+    //function with parameter
+
+    function squareNumber($num)
+    {
+        $square = $num * $num;
+        echo ("the square of $num is $square\n");
+    }
+    squareNumber(5);
 ?>
